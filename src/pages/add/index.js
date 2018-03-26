05d419b65859ff0e096fb8bc467acc38f521aebf
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-26 15:37:21 +0000

    Added add page

---
+++ src/pages/add/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import ExternalLink from '../../components/ExternalLink.js'
+import versions from './versions.json'
+import './index.css'
+
+class AddPage extends React.Component {
+  render() {
+    return (
+      <div id='add'>
+        <div className='header'>
+          <h1>Add Bastion to Discord</h1>
+          <p>Add Bastion to Discord, and give awesome perks to your Discord server.</p>
+          <p>You have two different options here, you can either add the public Bastion bot to your Discord server.<br />Or you can choose to host Bastion yourself, privately, on your own machine/server and be your own boss.</p>
+        </div>
+
+        <div className='container'>
+            {
+              versions.map((version, i) => {
+                return (
+                  <div className='version' key={i}>
+                    <ExternalLink to={version.url}>
+                      <div className='details'>
+                        <h4>{version.title}</h4>
+                        <p>{version.description}</p>
+                      </div>
+                    </ExternalLink>
+                  </div>
+                );
+              })
+            }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddPage
