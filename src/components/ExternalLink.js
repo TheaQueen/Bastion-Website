7e7cdad59f83f25492b00d6e649f0a64be424fe0
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-26 15:21:13 +0000

    Added ExternalLink react component

---
+++ src/components/ExternalLink.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ExternalLink extends React.Component {
+  render() {
+    return (
+      <a href={this.props.to} target='_blank'>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+export default ExternalLink;
