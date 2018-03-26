ad1f0f9fd76c429759718b483763aa965fe9c7a7
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-26 15:36:51 +0000

    Added sponsors page

---
+++ src/pages/sponsors/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ExternalLink from '../../components/ExternalLink.js'
+import sponsors from './sponsors.json'
+import './index.css'
+
+class SponsorsPage extends React.Component {
+  render() {
+    return (
+      <div id='sponsors'>
+        <div className='header'>
+          <h1>Sponsors</h1>
+          <p>These are the amazing companies and people who sponsor <strong>The Bastion Bot Project</strong> and keep us running.</p>
+        </div>
+
+        <div className='container'>
+            {
+              sponsors.map((sponsor, i) => {
+                return (
+                  <div className='sponsor' key={i}>
+                    <ExternalLink to={sponsor.url}>
+                      <div className='image'>
+                        <img src={sponsor.image} height='150' width='150' alt='Sponsor Logo' />
+                      </div>
+                      <div className='details'>
+                        <h4>{sponsor.title}</h4>
+                        <p>{sponsor.description}</p>
+                      </div>
+                    </ExternalLink>
+                  </div>
+                );
+              })
+            }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SponsorsPage
