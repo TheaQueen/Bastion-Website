750cca86f113a8773c3edd88791931a5fe7955e1
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-26 15:22:16 +0000

    Added NavLink react component

---
+++ src/components/NavLink.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+class NavLink extends React.Component {
+  render() {
+    return (
+      <li onMouseOver={this.props.onMouseOver} onMouseOut={this.props.onMouseOut}>
+        <Link to={this.props.to} className={this.props.className}>
+          {this.props.name}
+        </Link>
+        {this.props.children}
+      </li>
+    );
+  }
+}
+
+export default NavLink;
