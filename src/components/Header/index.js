dd2b96fe29c6c64b6ba29ff053beb35dcae47ec4
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-26 15:25:24 +0000

    Added Header react component

---
+++ src/components/Header/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import MainNav from '../MainNav/'
+import './index.css'
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavVisible: false,
+      width: 0,
+      height: 0
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({
+      isNavVisible: false,
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavVisible: !this.state.isNavVisible
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <div className='innerHeader'>
+          <Link to='/' onClick={() => this.toggleNav()}>
+            <div className='headerLogo'></div>
+          </Link>
+          <MainNav isNavVisible={this.state.width > 960 || this.state.isNavVisible} onClick={() => this.toggleNav()} />
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header
