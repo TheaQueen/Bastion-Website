0ba1ee54af2ab2a8e4407781797bda761b177405
Sankarsan Kampa <me@sankarsankampa.com>
2018-03-26 15:30:10 +0000

    Added default layout

---
+++ src/layouts/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import './index.css'
+
+class DefaultLayout extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <root>
+        <Helmet
+          title="The Bastion Bot"
+          meta={[
+            { name: 'description', content: 'Give awesome perks to your server!' },
+            { name: 'keywords', content: 'bastion, bastion bot, discord, discord bot, bot' },
+            { name: 'generator', content: 'Bastion' }
+          ]}
+        />
+        <Header />
+        <main>{this.props.children()}</main>
+        <Footer />
+      </root>
+    );
+  }
+}
+
+export default DefaultLayout
