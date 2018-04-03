e1ade5641815ad6e03dca43f7e0ad1f76828fbe2
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-04-03 19:43:47 +0000

    Externalize site meta

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import SiteBanner from '../components/SiteBanner'
 import BackToTop from '../components/BackToTop'
+import { siteMetadata } from '../../gatsby-config'
 import './index.css'
 
 class DefaultLayout extends React.Component {
@@ -15,19 +16,8 @@ class DefaultLayout extends React.Component {
     return (
       <root>
         <Helmet
-          title='The Bastion Bot - One of the best Discord Bot'
-          meta={[
-            { name: 'description', content: 'Give awesome perks to your server!' },
-            { name: 'keywords', content: 'bastion, bastion bot, discord, discord bot, bot, music, currency, the best discord bot, best discord bot, best bot, stream, game, fun, administration, moderation, queries, searches, gambling, game stats, stats, info' },
-            { name: 'generator', content: 'Bastion' },
-            { property: 'og:title', content: 'The Bastion Bot' },
-            { property: 'og:description', content: 'Give awesome perks to your Discord server!' },
-            { property: 'og:url', content: 'https://bastionbot.org' },
-            { property: 'og:image', content: 'https://resources.bastionbot.org/og-image.jpg' },
-            { property: 'og:image:width', content: '1000' },
-            { property: 'og:image:height', content: '524' }
-          ]}
-
+          title = {siteMetadata.title}
+          meta = {siteMetadata.meta}
         />
         <Header />
         <main>{this.props.children()}</main>
