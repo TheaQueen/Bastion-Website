97f04ec2a9e4f4193a0469a74941b71afaf3b897
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-04-03 19:41:57 +0000

    Updated config - Externalizing site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'The Bastion Bot',
-    description: 'Add Bastion to Discord and give awesome perks to your Discord server!',
-    generator: 'Bastion'
+    title: 'The Bastion Bot - One of the best Discord Bot',
+    meta: [
+      { name: 'description', content: 'Give awesome perks to your server!' },
+      { name: 'keywords',
+        content: 'bastion, bastion bot, discord, discord bot, bot, music,'
+          + ' currency, the best discord bot, best discord bot, best bot,'
+          + ' stream, game, fun, administration, moderation, queries,'
+          + ' searches, gambling, game stats, stats, info' },
+      { property: 'og:title', content: 'The Bastion Bot' },
+      { property: 'og:description',
+        content: 'Give awesome perks to your Discord server!' },
+      { property: 'og:url', content: 'https://bastionbot.org' },
+      { property: 'og:image',
+        content: 'https://resources.bastionbot.org/og-image.jpg' },
+      { property: 'og:image:width', content: '1000' },
+      { property: 'og:image:height', content: '524' }
+    ]
   },
   plugins: [
     'gatsby-plugin-react-helmet',
