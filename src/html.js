4774126742ca8757545768891cbfe79fc1dc1edd
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-04-04 10:02:13 +0000

    Set site meta from html.js

--- src/html.js
+++ src/html.js
@@ -32,6 +32,40 @@ class HTML extends React.Component {
               + ' initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0'
             }
           />
+
+          <title>The Bastion Bot - One of the best Discord Bot</title>
+          <meta
+            name='description'
+            content='Give awesome perks to your Discord server!'
+          />
+          <meta
+            name='keywords'
+            content={
+              'bastion, bastion bot, discord, discord bot, bot, music,'
+                + ' currency, the best discord bot, best discord bot, best bot,'
+                + ' stream, game, fun, administration, moderation, queries,'
+                + ' searches, gambling, game stats, stats, info'
+            }
+          />
+
+          <meta property='og:site_name' content='The Bastion Bot' />
+          <meta
+            property='og:title'
+            content='The Bastion Bot - One of the best Discord Bot'
+          />
+          <meta
+            property='og:description'
+            content='Give awesome perks to your Discord server!'
+          />
+          <meta property='og:url' content='https://bastionbot.org' />
+          <meta property='og:image:width' content='1000' />
+          <meta property='og:image:height' content='524' />
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:image'
+            content='https://resources.bastionbot.org/og-image.jpg'
+          />
+
           <link
             rel='icon'
             type='image/x-icon'
