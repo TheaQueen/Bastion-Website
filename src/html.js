765bf7987f16c0dfb10b84d6e18b5906b4293190
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-04-04 15:09:56 +0000

    Added Open Graph RDFa Core 1.1 CURIE prefix

--- src/html.js
+++ src/html.js
@@ -21,7 +21,7 @@ class HTML extends React.Component {
       );
     }
     return (
-      <html { ...this.props.htmlAttributes }>
+      <html { ...this.props.htmlAttributes } prefix='og: http://ogp.me/ns#'>
         <head>
           <meta charSet='utf-8' />
           <meta httpEquiv='x-ua-compatible' content='ie=edge' />
