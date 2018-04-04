a643334b1fcecf917cfca8e34110f31fc15e53e2
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-04-04 10:02:50 +0000

    Removed siteMetadata content form gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,5 @@
 module.exports = {
-  siteMetadata: {
-    title: 'The Bastion Bot - One of the best Discord Bot',
-    meta: [
-      { name: 'description', content: 'Give awesome perks to your server!' },
-      { name: 'keywords',
-        content: 'bastion, bastion bot, discord, discord bot, bot, music,'
-          + ' currency, the best discord bot, best discord bot, best bot,'
-          + ' stream, game, fun, administration, moderation, queries,'
-          + ' searches, gambling, game stats, stats, info' },
-      { property: 'og:title', content: 'The Bastion Bot' },
-      { property: 'og:description',
-        content: 'Give awesome perks to your Discord server!' },
-      { property: 'og:url', content: 'https://bastionbot.org' },
-      { property: 'og:image',
-        content: 'https://resources.bastionbot.org/og-image.jpg' },
-      { property: 'og:image:width', content: '1000' },
-      { property: 'og:image:height', content: '524' }
-    ]
-  },
+  siteMetadata: {},
   plugins: [
     'gatsby-plugin-react-helmet',
     {
