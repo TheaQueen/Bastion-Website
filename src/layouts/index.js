a236e94bec4f5809d796ca4a90df116c47f665bf
Sankarsan Kampa <me@sankarsankampa.com>
2018-04-13 14:59:49 +0000

    Some minor changes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,6 +8,12 @@ import './index.css';
 class DefaultLayout extends React.Component {
   constructor(props) {
     super(props);
+    console.log('%cHello!😄', 'color: #62d9fb; font-size: 100px;');
+    console.group('%c[SPECIAL INVITES]', 'color: #40c0fb; font-size: 1.5em;');
+    console.log('%cSince you\'re here. Here\'re some quick invite links.', 'font-size: 1.2em;');
+    console.log('%c[Bastion HQ]: %chttps://discord.gg/fzx8fkt', 'color: #62d9fb; font-size: 1.2em;', 'font-size: 1.2em;');
+    console.log('%c[Invite Bastion]: %chttps://discordapp.com/oauth2/authorize?client_id=267035345537728512&scope=bot&permissions=2146958463', 'color: #62d9fb; font-size: 1.2em;', 'font-size: 1.2em;');
+    console.groupEnd();
   }
 
   render() {
